f67287472ef247fe2c06977b1848870999af2b66
José Flórez <todoappiatest@gmail.com>
2025-07-12 22:54:26 -0500

    test

--- backend/tests/test_crud.py
+++ backend/tests/test_crud.py
@@ -52,3 +52,6 @@ def test_create_and_get_task():
     assert delete_resp.status_code == 200
     delete_data = delete_resp.json()
     assert delete_data["detail"] == "Task deleted"
+
+
+# esto es una prueba
